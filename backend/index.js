915daa3349a8f6f54aaf2c5f36f69bcd586bba51
pwds <thepwds@proton.me>
2026-03-18 22:31:45 -0400

    make basic hello world server

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const host = "localhost";
+const port = 8080;
+
+const requestListener = function(req, res) {
+    res.writeHead(200);
+    res.end("hello world");
+}
+
+const server = http.createServer(requestListener);
+server.listen(port,host,() => {
+    console.log("Server running");
+});
